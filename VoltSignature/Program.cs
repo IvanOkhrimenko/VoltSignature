1f0a4a4c58d63df1fa784543db5516cef16c06a5
Sergey Kopytov <skop@diceus.com>
2018-08-13 16:36:01 +0300

    Add kestrel integration

--- VoltSignature/Program.cs
+++ VoltSignature/Program.cs
@@ -20,6 +20,8 @@ namespace VoltSignature.UI
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
     }
