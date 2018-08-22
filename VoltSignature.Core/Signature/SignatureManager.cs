339b56b9f721686209e534d7f2d96ea49f21e6c4
Сергей Копытов <RevoPower@outlook.com>
2018-08-23 00:44:06 +0300

    Add base classes to signature and creating certificate

---
+++ VoltSignature.Core/Signature/SignatureManager.cs
@@ -0,0 +1,45 @@
+﻿using Org.BouncyCastle.Asn1.Pkcs;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoltSignature.Core.Signature
+{
+    public  class SignatureManager
+    {
+        public SignatureManager()
+        {
+            _signer = SignerUtilities.GetSigner(PkcsObjectIdentifiers.Sha512_256WithRSAEncryption.Id);
+        }
+
+        ISigner _signer;
+
+        public void InitPrivateKey(RsaKeyParameters privatKey)
+        {
+            _signer.Reset();
+            _signer.Init(true, privatKey);
+        }
+
+        public void InitPublicKey(RsaKeyParameters publicKey)
+        {
+            _signer.Reset();
+            _signer.Init(false, publicKey);
+        }
+         
+        public byte[] SignFile(byte[] file, RsaKeyParameters privatKey)
+        {
+            _signer.BlockUpdate(file, 0, file.Length);
+            byte[] signature = _signer.GenerateSignature();
+            return signature;
+        }
+
+        public bool VerifySignature(byte[] file, byte[] signature)
+        {
+            _signer.BlockUpdate(file, 0, file.Length);
+            return  _signer.VerifySignature(signature);
+        }
+    }
+}
---
+++ VoltSignature.Test/Certificate/CertificateTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using VoltSignature.Core.Certificate;
+
+namespace VoltSignature.Test.Certificate
+{
+    [TestClass]
+    public class CertificateTest
+    { 
+
+        [TestMethod]
+        public void TestCreateCertificate()
+        {
+            CertificateManager manager = new CertificateManager();
+            var KeyPair = manager.GeneratePair();
+            var certificate = manager.GenerateCertificate("Tom Holland", "ABC motors", KeyPair.Private, KeyPair.Public);
+            Assert.IsNotNull(certificate);
+            byte[] certBytes = manager.ConvertToByte(certificate);
+            Assert.IsNotNull(certBytes);
+            //var cert = build.GenerateKeyValuePair(); 
+            File.WriteAllBytes(@"cert.crt", certBytes);
+
+            X509Certificate certificateFromByte = new X509Certificate("cert.crt");
+            Assert.IsNotNull(certificateFromByte);
+
+        }
+         
+    }
+}
